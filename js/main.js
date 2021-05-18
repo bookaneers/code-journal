e40dc6e76d96721abe5f93ad46968875865642ed
Fernando Figueiredo <bookaneers@gmail.com>
2021-05-17 17:07:51 -0700

    Completed Issue 1.

--- js/main.js
+++ js/main.js
@@ -1,9 +1,22 @@
 /* global data */
-/* exported data */
-
-var $image = document.querySelector('img');
-
-var $photoUrl = document.querySelector('#photo-url');
+const $image = document.querySelector('img');
+const $photoUrl = document.querySelector('#photo-url');
+const $entryForm = document.querySelector('#contact-form');
 $photoUrl.addEventListener('input', function (event) {
   $image.setAttribute('src', event.target.value);
+});
+
+/* exported data */
+$entryForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const entry = {
+    title: $entryForm.elements.title.value,
+    photoURL: $entryForm.elements['photo-url'].value,
+    note: $entryForm.elements.notes.value,
+    entryId: data.nextEntryId
+  };
+  data.entries.unshift(entry);
+  data.nextEntryId++;
+  document.getElementById('contact-form').reset();
+  $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
