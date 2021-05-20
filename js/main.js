33ff92a5b5160cdab58f9fa37f88a693ed811426
Fernando Figueiredo <bookaneers@gmail.com>
2021-05-19 20:14:29 -0700

    Created list and checked the wireframe.

--- js/main.js
+++ js/main.js
@@ -12,11 +12,31 @@ $entryForm.addEventListener('submit', function (event) {
   const entry = {
     title: $entryForm.elements.title.value,
     photoURL: $entryForm.elements['photo-url'].value,
-    note: $entryForm.elements.notes.value,
+    notes: $entryForm.elements.notes.value,
     entryId: data.nextEntryId
+
   };
   data.entries.unshift(entry);
   data.nextEntryId++;
   document.getElementById('contact-form').reset();
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
+
 });
+
+// const $displayTitle = document.createElement('h3');
+// const $displayUrl = document.createElement('img');
+// const $displayNotes = document.createElement('p');
+
+// window.addEventListener('DOMContentLoaded', function (event) {
+
+//   console.log('DOM fully loaded and parsed');
+//   console.log(data.entries);
+//   for (let i = 0; i < data.entries.length; i++) {
+//     console.log(data.entries[i]);
+//     $displayTitle.append(data.entries[i].title);
+//     $displayUrl.append(data.entries[i].photoURL);
+//     $displayNotes.append(data.entries[i].notes);
+//   }
+// });
+
+// console.log(data);
