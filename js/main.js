ceea60be1064e4c845ae0f3dc03671154990ac32
Fernando Figueiredo <bookaneers@gmail.com>
2021-06-09 18:21:06 -0700

    Completed issue 3.

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 /* global data */
 /* exported data */
-
+// ---------------------------- ADDING AN ITEM ----------------------------
 // querying the DOM
 // placeholder image
 const $image = document.querySelector('img');
@@ -17,32 +17,57 @@ $photoUrl.addEventListener('input', function (event) {
 // function to add an entry to the database
 $contactForm.addEventListener('submit', function (event) {
   event.preventDefault();
-  // create object
-  const entry = {
-    title: $contactForm.elements.title.value,
-    photoURL: $contactForm.elements['photo-url'].value,
-    notes: $contactForm.elements.notes.value,
-    entryId: data.nextEntryId
-  };
-  // add object to array (database)
-  data.entries.unshift(entry);
-  // increment next entry id
-  // call newEntry function with the entry parameter and
-  // store it in a variable
-  const $item = newItem(entry);
-  $uList.prepend($item);
-  data.nextEntryId++;
+  // replacing an entry
+  if (data.editing !== null) {
+    const updatedEntry = {
+      title: $contactForm.elements.title.value,
+      photoURL: $contactForm.elements['photo-url'].value,
+      notes: $contactForm.elements.notes.value,
+      entryId: data.nextEntryId
+    };
+    const $id = data.editing.entryId;
+    data.entries.splice(data.length - $id, 1, updatedEntry);
+    // reset data.editing
+    data.editing = null;
+    const $entriesListUpdated = document.querySelector('ul');
+    $entriesListUpdated.innerHTML = '';
+    for (let i = 0; i < data.entries.length; i++) {
+      // call function to display entry
+      var $item = newItem(data.entries[i]);
+      // append entry to UL
+      $entriesListUpdated.appendChild($item);
+    }
+  } else {
+    // addind an new entry
+    // create object
+    const entry = {
+      title: $contactForm.elements.title.value,
+      photoURL: $contactForm.elements['photo-url'].value,
+      notes: $contactForm.elements.notes.value,
+      entryId: data.nextEntryId
+    };
+    // add object to array (database)
+    data.entries.unshift(entry);
+    // increment next entry id
+    // call newEntry function with the entry parameter and
+    // store it in a variable
+    const $item = newItem(entry);
+    $uList.prepend($item);
+    data.nextEntryId++;
+  }
+
   // reset form
   document.getElementById('contact-form').reset();
   // set image iin the form
   $image.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
-
+// ---------------------------- DISPLAY ITEMS ----------------------------
 // function to display entry
 function newItem(entry) {
   // create a new li
   const $listedItem = document.createElement('li');
   $listedItem.setAttribute('class', 'listed-item');
+
   // create a new div = listed-item
   const $row = document.createElement('div');
   $row.setAttribute('class', 'row');
@@ -56,12 +81,12 @@ function newItem(entry) {
   const $displayUrl = document.createElement('div');
   $displayUrl.setAttribute('class', 'display-url');
 
-  // create a div = display-url
+  // create a div = display-text
   const $displayText1 = document.createElement('div');
   $displayText1.setAttribute('class', 'display-text');
+  // create another div = display-text
   const $displayText2 = document.createElement('div');
   $displayText2.setAttribute('class', 'display-text');
-
   // create a image tag
   const $image = document.createElement('img');
   $image.setAttribute('src', entry.photoURL);
@@ -72,6 +97,8 @@ function newItem(entry) {
   const $icon = document.createElement('img');
   // $icon.setAttribute('class', 'pen');
   $icon.setAttribute('src', 'images/edit-icon.png');
+  $icon.setAttribute('class', 'pen');
+  $icon.setAttribute('data-id', entry.entryId);
   // create a p tag
   const $displayNotes = document.createElement('p');
   $displayNotes.textContent = entry.notes;
@@ -80,7 +107,6 @@ function newItem(entry) {
   $row.appendChild($columnHalf1);
   $columnHalf1.appendChild($displayUrl);
   $displayUrl.appendChild($image);
-
   $row.appendChild($columnHalf2);
   $columnHalf2.appendChild($displayText1);
   $displayText1.appendChild($displayTitle);
@@ -100,13 +126,41 @@ for (let i = 0; i < data.entries.length; i++) {
   // append entry to UL
   $uList.appendChild($item);
 }
+
+// ---------------------------- NAVIGATION ----------------------------
 // querying the DOM to clicking functions
 const $newEntry = document.querySelector('#new-entry');
 const $entriesView = document.querySelector('#entries-view');
+const $entriesList = document.querySelector('.entries-list');
 
 // data-views
 const $entryForm = document.querySelector('#entry-form');
 const $entries = document.querySelector('#entries');
+
+$entriesList.addEventListener('click', function (event) {
+  if (event.target.className !== 'pen') {
+    return;
+  }
+  $entryForm.className = 'view';
+  const $h2 = document.querySelector('h2');
+  $h2.textContent = 'Edit Entry';
+  $entries.className = 'view hidden';
+
+  const $entryNumber = event.target.getAttribute('data-id');
+  const $number = parseInt($entryNumber);
+
+  for (let i = 0; i < data.entries.length; i++) {
+    if ($number === data.entries[i].entryId) {
+      $image.setAttribute('src', data.entries[i].photoURL);
+      $contactForm.elements.title.value = data.entries[i].title;
+      $contactForm.elements['photo-url'].value = data.entries[i].photoURL;
+      $contactForm.elements.notes.value = data.entries[i].notes;
+      // make sure entry already exit
+      data.editing = data.entries[i];
+    }
+  }
+
+});
 
 $entriesView.addEventListener('click', function (event) {
   $entries.className = 'view';
@@ -115,5 +169,7 @@ $entriesView.addEventListener('click', function (event) {
 
 $newEntry.addEventListener('click', function (event) {
   $entryForm.className = 'view';
+  const $h2 = document.querySelector('h2');
+  $h2.textContent = 'New Entry';
   $entries.className = 'view hidden';
 });
